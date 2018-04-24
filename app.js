7ecceecd8690550e6c02bf7a39df849a1ac14ebd
Alex Mazhavy <alexander.mazhavy@gmail.com>
2018-04-23 23:31:25 -0400

    nunjucs working, connected to express.render

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan') // logging middleware
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+
+var locals = {
+    title : 'List of names',
+    people: [
+        {name: 'Jack'},
+        {name: 'John'}
+    ]
+};
+
+// nunjucks.render('index.html',locals,(err, output) => {
+//     if (err) return console.error(err);
+//     console.log(output)
+// });
+
+nunjucks.configure('views', {noCache: true}); // where to find views, cache off to see if generator works
+app.set('view engine', 'html'); // what file extension do our template have
+app.engine('html', nunjucks.render); // how to render html templates
 
 app.use(morgan('dev'));
 
 app.get('/',(req, res) => {
-    res.send(chalk.red('Hello World!\n'));
-})
+    res.render('index', locals)
+});
 
 app.get('/news',(req, res, next) => {
     res.json({name: 'alex', lastName: 'May'});
     next();
 })
 
-app.listen('1337',() => console.log('Server is up...'));
+app.listen('3000',() => console.log('Server is up...'));
