85696950909ee0ee6d172727f5012aed8823214f
Alex Mazhavy <alexander.mazhavy@gmail.com>
2018-04-23 23:14:10 -0400

    working express app. Morgan logger

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan') // logging middleware
 const chalk = require('chalk');
 
+app.use(morgan('dev'));
+
 app.get('/',(req, res) => {
-    res.send('Hello World!');
+    res.send(chalk.red('Hello World!\n'));
 })
 
-app.get('/news',(req, res) => {
-    res.send('News route');
+app.get('/news',(req, res, next) => {
+    res.json({name: 'alex', lastName: 'May'});
+    next();
 })
 
-
-app.listen('3000',() => console.log('Server is up...'));
+app.listen('1337',() => console.log('Server is up...'));
